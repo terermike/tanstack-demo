b1bff63e4ce897a27e379c71b3f19e0465caaf4d
Terer <terermike3@gmail.com>
2024-08-13 14:23:30 +0300

    Modified file to use query client and provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,36 @@ import {
   Navigate,
   RouterProvider,
   createBrowserRouter,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import Events from './components/Events/Events.jsx';
-import EventDetails from './components/Events/EventDetails.jsx';
-import NewEvent from './components/Events/NewEvent.jsx';
-import EditEvent from './components/Events/EditEvent.jsx';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Events from "./components/Events/Events.jsx";
+import EventDetails from "./components/Events/EventDetails.jsx";
+import NewEvent from "./components/Events/NewEvent.jsx";
+import EditEvent from "./components/Events/EditEvent.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Navigate to="/events" />,
   },
   {
-    path: '/events',
+    path: "/events",
     element: <Events />,
 
     children: [
       {
-        path: '/events/new',
+        path: "/events/new",
         element: <NewEvent />,
       },
     ],
   },
   {
-    path: '/events/:id',
+    path: "/events/:id",
     element: <EventDetails />,
     children: [
       {
-        path: '/events/:id/edit',
+        path: "/events/:id/edit",
         element: <EditEvent />,
       },
     ],
@@ -38,7 +39,12 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+    </QueryClientProvider>
+  );
 }
 
 export default App;
