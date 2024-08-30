61d49000c07bd0d27469f2a5e138b9c1f4541bf3
Terer <terermike3@gmail.com>
2024-08-30 17:43:40 +0300

    Imported reactClient instance

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import Events from "./components/Events/Events.jsx";
 import EventDetails from "./components/Events/EventDetails.jsx";
 import NewEvent from "./components/Events/NewEvent.jsx";
 import EditEvent from "./components/Events/EditEvent.jsx";
+import { queryClient } from "./util/http.js";
 
 const router = createBrowserRouter([
   {
@@ -39,7 +40,6 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />;
